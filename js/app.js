5d03a56b4e66ee92f12ea62b208e92c01c126103
Despoina Tosounidou <despoina.tosounidou@gmail.com>
2017-12-27 21:21:13 +0200

    added hover effect on button

--- js/app.js
+++ js/app.js
@@ -4,7 +4,10 @@ const addItem = document.querySelector('.aItem');
 addItem.addEventListener('click', () => {
     let ul = document.getElementsByTagName('ul')[0];
     let li = document.createElement('li');
+    let img = document.querySelector('.notodos')
     li.textContent = newItem.value;
     ul.appendChild(li);
     newItem.value = '';
+    img.style.display = 'none';
+
 });
