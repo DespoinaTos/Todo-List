978dc1122c89f6f63ace011a52ab3ab431414005
Despoina Tosounidou <despoina.tosounidou@gmail.com>
2017-12-28 15:17:27 +0200

    added buttons to every new todo

--- js/app.js
+++ js/app.js
@@ -5,7 +5,16 @@ addItem.addEventListener('click', () => {
     let ul = document.getElementsByTagName('ul')[0];
     let li = document.createElement('li');
     let img = document.querySelector('.notodos')
+    let buttonDiv = document.createElement('div');
+    let completed = document.createElement('i');
+    completed.className = 'fa fa-check-square done';
+    let remove = document.createElement('i');
+    remove.className = 'fa fa-trash-o remove';
     li.textContent = newItem.value;
+    buttonDiv.className = 'fontbuttons';
+    buttonDiv.appendChild(remove);
+    buttonDiv.appendChild(completed);
+    li.appendChild(buttonDiv);
     ul.appendChild(li);
     newItem.value = '';
     img.style.display = 'none';
